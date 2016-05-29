0bb95568a1edcd5ca63d74e6d8dba7903c596336
Kacper Kosikowski <kosikowski.kacper@gmail.com>
2016-05-29 21:03:52 +0200

    Wall is not a line anymore. It is a face now.

--- Matlab/scene/room/Wall3d.m
+++ Matlab/scene/room/Wall3d.m
@@ -1,9 +1,9 @@
-classdef Wall2d < handle
+classdef Wall3d < handle
     %--------------
     % Short description
     %--------------
     properties (GetAccess = 'private', SetAccess = 'private')
-        line;
+        face;
         material;
         id;
     end
@@ -16,35 +16,26 @@ classdef Wall2d < handle
     %--------------
     methods (Access = 'public')
         %Constructor
-        function this = Wall2d(originVertex, endVertex, material, id)
+        function this = Wall3d(face, material, id)
             if (nargin > 0)
-                this.line = Line2d(originVertex, endVertex, material.getColor());
+                this.face = face;
                 this.material = material;
                 this.id = id;
             end
         end
         
         function draw(this, drawing2dContext)
-            Wall2d.validateInput(drawing2dContext);
-            this.line.draw(drawing2dContext);
+            %Wall2d.validateInput(drawing2dContext);
+            %this.line.draw(drawing2dContext);
+            error('Drawing is not supported in 3D mode.');
         end
         
         %Getters
-        function line = getLine(this)
-            line = this.line;
+        function line = getFace(this)
+            line = this.face;
         end
         function material = getMaterial(this)
             material = this.material;
-        end
-    end
-    %--------------
-    %Private Methods
-    %--------------
-    methods (Access = 'private', Static = true)
-        function validateInput(drawing2dContext)
-            if ~isa(drawing2dContext, 'Drawing2dContext')
-                error('Invalid input argument');
-            end
         end
     end
 end
